aa17b8f1058509d6331c74149ee86c4c79d2098c
HandyLukita1 <handylukita99@gmail.com>
2026-09-23 22:02:29 +0700

    delete code worksapce

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,3 +1,29 @@
 import { Outlet } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
+export default function Layout() {
+    return (
+        <div style={{
+            display: 'flex',
+            flexDirection:'column',
+            minHeight: '100vh',
+            fontFamily: 'system-ui,sans-serif'
+        }}>
+         
+         {/* Now is Header File*/}
+         <header style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            padding: '1rem 3rem',
+            backgroundColor: '#ffffff',
+            borderBottom: '1px solid #e2e8f0',
+            position: 'sticky',
+            top: 0,
+            zIndex: 1000
+         }}>
+            
+         </header>
+        </div>
+    )
+}
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,3 +4,40 @@ import asset1 from '../assets/Asset-01.svg';
 import asset2 from '../assets/Asset-02.svg';
 import asset3 from '../assets/Asset-04.svg';
 import asset4 from '../assets/Asset-03.svg';
+
+export default function Home() {
+   return (
+       <div style={{ 
+        minHeight: '88vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor: '#f8fbff',
+        overflowX: 'hidden' }}>
+     
+      {/*Now is the cover section*/}
+      <div style={{ 
+          width: '100%',
+          backgroundColor: '#ffffff',
+          padding: '4rem 2rem',
+          borderBottom: '1px solid #e2e8f0' }}>
+        
+        <div style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat (auto-fit, minmax(300px,1fr)',
+            gap: '3rem',
+            margin: '0 auto',
+            maxWidth: '1200px',
+            alignItems: 'center'
+        }}>
+
+        </div>
+
+
+    </div>
+        
+
+
+</div>
+    )
+}
