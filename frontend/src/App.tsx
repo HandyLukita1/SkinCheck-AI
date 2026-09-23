d5d366961203ce3927fc45e29223804905094e8b
HandyLukita1 <handylukita99@gmail.com>
2026-09-23 20:49:18 +0700

    added react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,11 @@ import Home from './pages/Home.tsx';
 import Scan from './pages/Scan.tsx';
 
 
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/scan" element={<Scan />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+   
   );
 }
+
 
 export default App;
