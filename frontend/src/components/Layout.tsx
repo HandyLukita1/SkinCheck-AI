cf5fc2bfe64bb1a61c248fbbf91be6014b815b29
HandyLukita1 <handylukita99@gmail.com>
2026-09-26 22:51:21 +0700

    Added a text and configure a style

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -56,11 +56,72 @@ export default function Layout() {
               >Scanner</Link>
         </nav>
       </header>
+      
+      {/* Main Page */}
+      <main style = {{ flex: 1}}>
+        <Outlet />
+      </main>
+
+      {/* Footer */}
+      <footer style = {{
+        backgroundColor: '#d3d3d3',
+        color: '#94a3b8',
+        padding: '2.5rem 3rem',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        gap: '1rem',
+        borderTop: '1px solid #334155' 
+      }}>
+
+        <div>
+          <p style = {{
+            color: '#ffffff',
+            fontWeight: 700,
+            fontSize: '1.1rem',
+            margin: '0 0 0.25rem 0' }}
+            >SkinCheckAI</p>
+          <p style = {{
+            color: '#ffffff',
+            fontWeight: 700,
+            fontSize: '1.1 rem',
+            margin: '0.25 rem'
+          }}> Your Dermatologist Solution, where we provide details
+              about your skin like normal mole and bad mole
+          </p>
+        </div>
+
+        <div style ={{
+          maxWidth: '400px',
+          maxHeight: '600px'
+        }}>
+          <p style = {{
+            color: '#ffffff',
+            fontWeight: 500,
+            fontSize: '1.3 rem',
+            margin: '0.22 rem'
+          }}> This is an assistive tool, not a change for professional</p>
+        </div>
+
+      
+
+        
+        
+
+
+        
+  
+
+      </footer>
+
+
+
+      
 
 
 
             
-         </header>
-        </div>
-    )
+         </div>
+    );
 }
